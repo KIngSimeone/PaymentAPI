122ffa6daff25256b3da723199ffeb5c0b9e38a2
Kingsimeone <simeoneumoh@outlook.com>
2021-01-21 13:11:38 +0100

    SQL Alchemy installed

--- application/app.py
+++ application/app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
+
+
+
 
 
 if __name__ == '__main__':
