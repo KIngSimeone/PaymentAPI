87c558de4cb8aa1f429e83173b21175cac2a919e
Kingsimeone <simeoneumoh@outlook.com>
2021-01-22 17:54:56 +0100

    sample resource created

--- application/app.py
+++ application/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {"Hello World"}
+
+api.add_resource(Helloworld, "/hello")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
