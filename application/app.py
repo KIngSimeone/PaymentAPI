d307e1638c5bde53d5525968777215e74f696e72
Kingsimeone <simeoneumoh@outlook.com>
2021-01-22 18:03:57 +0100

    pass extra data in url

--- application/app.py
+++ application/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return {"Hello World"}
+    def get(self, name):
+        return {"data": name}
 
-api.add_resource(Helloworld, "/hello")
+api.add_resource(HelloWorld, "/hello/<string:name>")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
