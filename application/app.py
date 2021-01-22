18f186e6fef28a7f0258b03619a0edc88d77d01e
Kingsimeone <simeoneumoh@outlook.com>
2021-01-22 19:55:34 +0100

    create payment method complete

--- application/app.py
+++ application/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, fields
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 
 
 # Initialize flask app
@@ -25,37 +25,50 @@ class TransactionRecord(db.Model):
     expiryDate = db.Column(db.Date,nullable=False)
     securityCode = db.Column(db.String(20),nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    createdAt = db.Column(db.Date,nullable=False)
 
     def __repr__(self):
         ccn = str(self.CardHolder + "'s Transaction")
         return ccn
 
-db.create_all()
 
-"""
 # create serialization for fields
 payment_fields = {
     'id': fields.Integer,
-    'name': fields.String,
-
+    'creditCardNumber': fields.String,
+    'cardHolder': fields.String,
+    'expiryDate': fields.String,
+    'securityCode': fields.String,
+    'amount':fields.Float,
+    'createdAt': fields.String
 }
-"""
+
 
 
 #   check if required fields in request body of Payment method
 paymentFields = reqparse.RequestParser()
-paymentFields.add_argument("CreditCardNumber", type=str, help= "CreditCardNumber", required=True)
-paymentFields.add_argument("CardHolder", type=str, help= "CardHolder is required", required=True)
-paymentFields.add_argument("ExpirationDate", type=str, help= "ExpirationDate is required", required=True)
-paymentFields.add_argument("SecurityCode", type=str, help= "SecurityCode is required", required=True)
-paymentFields.add_argument("CardHolder", type=str, help= "CardHolder is required", required=True)
-paymentFields.add_argument("Amount", type=float, help= "Amount is required", required=True)
+paymentFields.add_argument("CreditCardNumber", type=str, help= "CreditCardNumber")
+paymentFields.add_argument("CardHolder", type=str, help= "CardHolder is required")
+paymentFields.add_argument("ExpirationDate", type=str, help= "ExpirationDate is required")
+paymentFields.add_argument("SecurityCode", type=str, help= "SecurityCode is required")
+paymentFields.add_argument("Amount", type=float, help= "Amount is required")
+
 
 class PaymentMethod(Resource):
+    # serialize fields with marshal_with payment fields
+    @marshal_with(payment_fields)
+
+    # create post request for payment API
     def post(self):
         args = paymentFields.parse_args()
-        return {"data":args}
+
+        date_time_obj = datetime.datetime.strptime(args['ExpirationDate'], '%Y-%m-%d')
+        # create transaction record
+        createdTransactionRecord = TransactionRecord(creditCardNumber=args['CreditCardNumber'], 
+                                                    cardHolder=args['CardHolder'], expiryDate=date_time_obj,
+                                                    securityCode=args['SecurityCode'], amount=args['Amount'])
+        db.session.add(createdTransactionRecord)
+        db.session.commit()
+        return createdTransactionRecord, 200
 
 api.add_resource(PaymentMethod,"/payment")
 
