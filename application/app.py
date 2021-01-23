e2050f750be5a042cd34846e28eaf1db1ad185c4
Kingsimeone <simeoneumoh@outlook.com>
2021-01-23 19:20:58 +0100

    API Complete

--- application/app.py
+++ application/app.py
@@ -3,7 +3,6 @@ from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
-
 # Initialize flask app
 app = Flask(__name__)
 
